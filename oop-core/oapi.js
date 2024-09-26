2669c4d165cbbec24f1280200a86b8ffc28230a7
跨下之辱 <1764194623@qq.com>
2024-09-26 11:36:45 +0800

    去除类型验证

--- oop-core/oapi.js
+++ oop-core/oapi.js
@@ -90,7 +90,7 @@ export function createInstance(className, data) {
             //是id直接赋值
             obj[k] =typeof data[k]=='object'?createInstance(k, data[k]):data[k]
         } else if (data?.[k]) {
-            if (typeof obj[k]!=typeof data?.[k])throw `${k} type err`
+            //if (typeof obj[k]!=typeof data?.[k])throw `${k} type err`
             //解决sql注入
             if (typeof data?.[k] =="string"){
                 data[k]=data?.[k].replaceAll(`'`,`"`)
@@ -119,7 +119,7 @@ export function createInstanceAndReq(className, json) {
                 obj[k] =typeof data[k]=='object'?createInstance(k, data[k]):data[k]
             } else if (data?.[k]) {
                 console.log(typeof obj[k],typeof data?.[k])
-                if (typeof obj[k]!=typeof data?.[k])throw `${k} type err`
+                //if (typeof obj[k]!=typeof data?.[k])throw `${k} type err`
                 //解决sql注入
                 if (typeof data?.[k] =="string"){
                     data[k]=data?.[k].replaceAll(`'`,`"`)
@@ -136,7 +136,7 @@ export function createInstanceAndReq(className, json) {
             } else if (json[k] && typeof v == 'object') {
                 obj[k] = createInstance(k, json[k])
             } else if (json?.[k]) {
-                if (typeof obj[k]!=typeof data?.[k])throw `${k} type err`
+                //if (typeof obj[k]!=typeof data?.[k])throw `${k} type err`
                 if (typeof json?.[k] =="string"){
                     json[k]=json?.[k].replaceAll(`'`,`"`)
                 }
