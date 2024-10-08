0db09b1326599b73b8f9ab275b7d63b5b8bdd654
跨下之辱 <1764194623@qq.com>
2024-10-08 14:04:16 +0800

    云对象页面更新

--- api/Attr.ts
+++ api/Attr.ts
@@ -7,8 +7,9 @@ export class Attr extends Base<Attr> {
     @Col({tag:'类型',sel:['字符串','数字'],type:'',show:'1111'})//1111代表增删改查是否显示
     type=0
     @Col({tag:'下拉框',type:'',show:'1111'})//1111代表增删改查是否显示
+    // @ts-ignore
     sel=''
-    @Col({tag:'是否筛选',sel:['是','否'],type:'',show:'1111'})//1111代表增删改查是否显示
+    @Col({tag:'是否筛选',sel:['否','是'],type:'',show:'1111'})//1111代表增删改查是否显示
     filter=0
     @Col({tag:'对象关系',sel:['1对1','1对多','多对1','多对多'],type:'',show:'1111'})//1111代表增删改查是否显示
     link=''
--- api/CloudObj.ts
+++ api/CloudObj.ts
@@ -1,15 +1,33 @@
 import {Base,Col,Menu} from "../oop-core/Base";
 import {classMap} from "../oop-core/oapi.js";
 import {migrate} from "../oop-core/migrate";
+import {Manager} from "./Manager";
 
-@Menu("云对象0代码")
-export class NoCode extends Base<NoCode> {
+@Menu("云对象")
+export class CloudObj extends Base<CloudObj> {
     @Col({tag:'云对象名称',type:'',filter:true,show:'1111'})//1111代表增删改查是否显示
     name=''
     @Col({tag:'菜单名',type:'',filter:false,show:'1111'})//1111代表增删改查是否显示
     menu=''
     @Col({tag:'云对象属性',link:'1n',show:'1111'})//1111代表增删改查是否显示
     attr=[]
+    async gets(r,r1) {
+        console.log(r,r1.socket.remoteAddress)
+        return Object.keys(classMap).map((x,i)=>{return {id:i+1,name:x,menu:new classMap[x]().constructor.menu}})
+    }
+
+    // @ts-ignore
+    async getpage() {
+        return Object.keys(classMap).map((x,i)=>{return {id:i+1,name:x,menu:new classMap[x]().constructor.menu}})
+    }
+    async del(id) {
+        let clazz=Object.keys(classMap)[id-1]
+        console.log(clazz)
+        const fs = require('fs').promises;
+        await fs.unlink(`./api/${upper(clazz)}.ts`)
+        await this.query(`drop table ${clazz}`)
+        return 1
+    }
     async add() {
         console.log('index',this)
         let sellist=[`''`,0]
@@ -23,7 +41,7 @@ export class NoCode extends Base<NoCode> {
 export class ${upper(this.name)} extends Base<${upper(this.name)}> {
 ${attrs}
 }
-`);
+`);     // @ts-ignore
         const module = await import(`./${upper(this.name)}.ts`);
         // @ts-ignore
         classMap[this.name.toLowerCase()] = module[upper(this.name)]
